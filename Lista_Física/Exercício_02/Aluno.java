4d343ef98cec0f35f2854f01858c2fcd21229d99
mell <126731231+mellcs@users.noreply.github.com>
2024-09-03 22:07:42 -0300

    Create Aluno.java

---
+++ Lista_Física/Exercício_02/Aluno.java
@@ -0,0 +1,13 @@
+public class Aluno {
+    int matricula;
+    String nome;
+    int idade;
+    double altura;
+
+    public Aluno(int matricula, String nome, int idade, double altura) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+}
