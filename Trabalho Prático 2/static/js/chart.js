07e40ed89918e930821ec989f67e367fca51a051
mell <126731231+mellcs@users.noreply.github.com>
2024-11-25 15:36:37 -0300

    Create chart.js

---
+++ Trabalho Prático 2/static/js/chart.js
@@ -0,0 +1,40 @@
+function renderChart(labels, data) {
+    const ctx = document.getElementById('chartGenero').getContext('2d');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Jogos por Gênero',
+                data: data,
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    display: true,
+                    position: 'top'
+                },
+                tooltip: {
+                    callbacks: {
+                        label: function(tooltipItem) {
+                            return `Quantidade: ${tooltipItem.raw}`;
+                        }
+                    }
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        stepSize: 1
+                    }
+                }
+            }
+        }
+    });
+}
